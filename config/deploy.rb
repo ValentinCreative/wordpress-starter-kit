43b7787586e34997a6f48eededa4811c69115e6d
Valentin LÉVÊQUE <valentin@creativegang.fr>
2013-11-15 10:46:33 +0100

    edit capistrano files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,46 +1,39 @@
- # The project repo name. (one word: no spaces, dashes, or underscores)
-set :application, "test" #FIXME
+# Définissez le nom de votre application
+set :application, "NOM_APP"
+# Définissez le chemin de votre repo
+set :repository,  "git@github.com:USER/REPO.git"
 
-# Legacy from porting from drupal, best to just leave this as "default"
+# Définissez le chemin vers lequel vous voulez déployer votre WP
+set :deploy_to, "~/www/"
+
 set :domains, ["default"]
 
-# Set the repository type and location to deploy from.
-set :scm, :git
-set :repository,  "git@github.com:kronda/wp_capistrano.git" #FIXME
-# set :scm, :subversion
-# set :repository,  "https://svn.example.com/svn/#{application}/trunk/"
-# set(:scm_password) { Capistrano::CLI.password_prompt("SCM Password: ") }
+set :httpd_group, 'user' # généralement votre nom d'utilisateur chez votre héberger 
 
-# Set the database passwords that we'll use for maintenance. Probably only used
-# during setup.
-set(:db_root_pass) { Capistrano::CLI.password_prompt("Production Root MySQL password: ") }
-set(:db_pass) { random_password }
+set :scm, 'git'
+set :deploy_via, :copy
 
-# The subdirectory within the repo containing the DocumentRoot.
-set :app_root, "webroot"
-set :local_path, "/Path/to/your/local-folder" #FIXME
+# Définissez vos données de connection SQL
+set :db_user, 'DB_USER' 
+set(:db_pass) { Capistrano::CLI.password_prompt("Mot de passe SQL : ") } #Ne touchez pas, le mot de passe vous sera demandé lors du déploiement 
+set :db_name, 'DB_NAME'
+set :db_host, 'DB_HOST'
 
-# Use a remote cache to speed things up
-set :deploy_via, :remote_cache
-ssh_options[:user] = 'username' #FIXME
+set :stages, %w(dev prod) #définissez vos branches
+set :default_stage, "prod" #définissez votre branche par défaut
 
-# Multistage support - see config/deploy/[STAGE].rb for specific configs
-set :default_stage, "dev"
-set :stages, %w(dev staging prod)
-
-# Generally don't need sudo for this deploy setup
+set :ssh_options, {:user => 'SSH_USERNAME'} # définissez votre nom d'utilisateur ssh
 set :use_sudo, false
 
-# This allows the sudo command to work if you do need it
-default_run_options[:pty] = true
+# Définissez le chemin de votre WP sur votre machine
+set :app_root, "/Users/username/Sites/app"
+set :local_path, "/Users/username/Sites/app" 
 
-# Override these in your stage files if your web server group is something other than apache
-set :httpd_group, 'apache'
+# Définissez l'adresse de votre serveur (ici un exemple avec AlwaysData)
+role :web, "ssh.alwaysdata.com"  #HTTP server, Apache/etc
+role :app, "ssh.alwaysdata.com"  # Souvent le même que web
+role :db,  "ssh.alwaysdata.com", :primary => true 
 
+# Le chemin vers WP-cli
+set :wp, "cd #{current_path}/#{app_root} ; /home/user" 
 
-#set this to where you have installed your wp site locally (http://[local_domain]/wp-admin/ should exist. )
-<<<<<<< HEAD
-set :local_domain, 'localhost.com' #FIXME
-=======
-set :local_domain, 'local.domain.com'
->>>>>>> 4201a433b092ad8658bc1e117f1b2ba6fdc2c0ad
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,18 +1,4 @@
-# Set the deployment directory on the target hosts.
-set :deploy_to, "/home/username/sites/#{application}-#{stage}" #FIXME
+# Définissez le chemin vers votre votre WP de Dev
+set :deploy_to, "/home/user/www/#{application}/#{stage}"
 
-# Use the correct branch on github. Uncomment this if you have set up seperate branches for each staging area
-set :branch, "dev" #FIXME
-
-# The hostnames to deploy to.
-role :web, "yourdomain.com" #FIXME
-
-# Specify one of the web servers to use for database backups or updates.
-# This server should also be running Wordpress.
-role :db, "yourdomain.com", :primary => true #FIXME
-
-# The path to wp-cli
-set :wp, "cd #{current_path}/#{app_root} ; /usr/bin/wp" #FIXME (Should be the full path to the wp command on your server)
-
-# The username on the target system, if different from your local username
-ssh_options[:user] = 'username' #FIXME
+set :branch, "dev" 
