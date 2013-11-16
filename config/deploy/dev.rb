c340ee74b7276171d650e6dff08e969c237d1091
Valentin LÉVÊQUE <valentin@creativegang.fr>
2013-11-16 12:16:21 +0100

    edit deploy to path

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,4 @@
 # Définissez le chemin vers votre votre WP de Dev
-set :deploy_to, "/home/user/www/#{application}/#{stage}"
+set :deploy_to, "/home/user/www/#{application}-#{stage}"
 
 set :branch, "dev" 
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,4 @@
 # Définissez le chemin vers votre votre WP de Prod
-set :deploy_to, "/home/user/www/#{application}/#{stage}"
+set :deploy_to, "/home/user/www/#{application}-#{stage}"
 
 set :branch, "prod" 
