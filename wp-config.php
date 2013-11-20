a7a4fb6bc5a714f25e9f8932bfad29fb1260a2fb
LÉVÊQUE Valentin <contact@valentinleveque.me>
2013-11-20 16:06:19 +0100

    improve wp-config

--- wp-config.php
+++ wp-config.php
@@ -1,46 +1,57 @@
 <?php
-// ===================================================
-// Load database info and local development parameters
-// ===================================================
+// ====================================================================================
+// Charger les informations de base de données et les paramètres de développement local
+// ====================================================================================
 if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+	// ================================
+	// Paramètres pour la partie locale
+	// ================================
 	ini_set( 'display_errors', 1 );
 	define( 'WP_DEBUG_DISPLAY', true );
 	define( 'WP_LOCAL_DEV', true );
 	include( dirname( __FILE__ ) . '/local-config.php' );
 } elseif (	file_exists( dirname( __FILE__ ) . '/dev-config.php' ) ) {
+	// ==================================================
+	// Paramètres pour le deploiement de la branche "dev"
+	// ==================================================
 	ini_set( 'display_errors', 1 );
 	define( 'WP_DEBUG_DISPLAY', true );
-	define( 'WP_LOCAL_DEV', true );
+	define( 'WP_LOCAL_DEV', false );
 	include( dirname( __FILE__ ) . '/dev-config.php' );
 } elseif ( file_exists( dirname( __FILE__ ) . '/prod-config.php' ) ) {
+	// ===================================================
+	// Paramètres pour le deploiement de la branche "prod"
+	// ===================================================
 	ini_set( 'display_errors', 0 );
 	define( 'WP_DEBUG_DISPLAY', false );
 	define( 'WP_LOCAL_DEV', false );
 	include( dirname( __FILE__ ) . '/prod-config.php' );
 } else {
+	ini_set( 'display_errors', 0 );
+	define( 'WP_DEBUG_DISPLAY', false );
 	define( 'WP_LOCAL_DEV', false );
 	define( 'DB_NAME', '%%DB_NAME%%' );
 	define( 'DB_USER', '%%DB_USER%%' );
 	define( 'DB_PASSWORD', '%%DB_PASSWORD%%' );
-	define( 'DB_HOST', '%%DB_HOST%%' ); // Probably 'localhost'
+	define( 'DB_HOST', '%%DB_HOST%%' );
 }
 
-// ========================
-// Custom Content Directory
-// ========================
+// ===============================
+// Répertoire content personnalisé
+// ===============================
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
 
 // ================================================
-// You almost certainly do not want to change these
+// Vous n'avez normalement pas à changer ceci
 // ================================================
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-// ==============================================================
-// Salts, for security
-// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
-// ==============================================================
+// ===============================================================
+// Pour la sécurité
+// Trouvez les ici : https://api.wordpress.org/secret-key/1.1/salt
+// ===============================================================
 define( 'AUTH_KEY',         'put your unique phrase here' );
 define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
 define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
@@ -50,10 +61,10 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
-// ==============================================================
-// Table prefix
-// Change this if you have multiple installs in the same database
-// ==============================================================
+// =============================================================================
+// Préfix des tables
+// Changer ceci si vous avez plusieurs installations sur la même base de données
+// =============================================================================
 $table_prefix  = 'wp_';
 
 // ================================
@@ -62,30 +73,35 @@ $table_prefix  = 'wp_';
 // ================================
 define('WPLANG', 'fr_FR');
 
-// ===========
-// Hide errors
-// ===========
+// ===================
+// Masquer les erreurs
+// ===================
 ini_set( 'display_errors', 0 );
 define( 'WP_DEBUG_DISPLAY', false );
 
-// =================================================================
+// ===========================
+// Augmenter la limite mémoire
+// ===========================
+define('WP_MEMORY_LIMIT', '100M');
+
+// ==============================
 // Debug mode
-// Debugging? Enable these. Can also enable them in local-config.php
-// =================================================================
+// Debugging? Activez ces lignes.
+// ==============================
 // define( 'SAVEQUERIES', true );
 // define( 'WP_DEBUG', true );
 
-// ======================================
-// Load a Memcached config if we have one
-// ======================================
+// ===============================================
+// Charger la config Memcached si vous en avez une
+// ===============================================
 if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
 	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
 
-// ===========================================================================================
-// This can be used to programatically set the stage when deploying (e.g. production, staging)
-// ===========================================================================================
+// ==========================================================================================================
+// Cela peut être utilisé pour définir par programmation la branche lors du déploiement (ex: production, dev)
+// ==========================================================================================================
 define( 'WP_STAGE', '%%WP_STAGE%%' );
-define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
+define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' );
 
 // ===================
 // Bootstrap WordPress
